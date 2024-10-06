3a5e0d6c288ad96721f65f6f527f49a84fc6b3b8
MagnusNaessanGaarder <magnusng@stud.ntnu.no>
2024-10-07 01:03:32 +0200

    Lagde filstrukturen som ble spesifisert av faglærer

--- test/java/test.java
+++ test/java/test.java
@@ -1,5 +1,7 @@
+package test.java;
+
 public class test {
     public static void main(String[] args) {
-        System.out.println("hei verden!");
+        System.out.println("Hei verden!");
     }
 }
