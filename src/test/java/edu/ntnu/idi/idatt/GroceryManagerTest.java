3a646bec46212ba037978967fb4f56eaf830092c
MagnusNaessanGaarder <magnusng@stud.ntnu.no>
2024-11-29 17:53:39 +0100

    Added new JUnit test - GroceryManagerTest.

---
+++ src/test/java/edu/ntnu/idi/idatt/GroceryManagerTest.java
@@ -0,0 +1,22 @@
+package edu.ntnu.idi.idatt;
+
+import org.junit.jupiter.api.Test;
+
+class GroceryManagerTest {
+
+  @Test
+  void getAmountAndUnit() {
+  }
+
+  @Test
+  void getPricePerQuantity() {
+  }
+
+  @Test
+  void addAmountGrocery() {
+  }
+
+  @Test
+  void removeAmountGrocery() {
+  }
+}
